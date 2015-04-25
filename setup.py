cab83a873efeb208af019055a1b61d87dbfd939e
Kevin Ross <me@kevinross.name>
2015-04-25 19:51:12 -0400

    rename to avoid conflict

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-setup(name='jsonrpc',
+setup(name='jsonrpcrmi',
       version='1.0',
-      description='implementation of JSON-RPC v2 client and server',
+      description='implementation of JSON-RPC v2 client and server inspired by Java\'s RMI',
       url='https://github.com/kevinross/jsonrpc',
       author='Kevin Ross',
       author_email='me@kevinross.name',
       license='GPLv2',
-      package_dir={'jsonrpc':'src/main/python/jsonrpc'},
-      packages=['jsonrpc'],
+      package_dir={'jsonrpcrmi':'src/main/python/jsonrpc'},
+      packages=['jsonrpcrmi'],
       install_requires=['bottle',
                         'requests',
                         'simplejson'],
---
+++ src/main/python/jsonrpcrmi/__init__.py
@@ -0,0 +1,1 @@
+from jsonrpcrmi import *
